5075d8ef475da338551766356bdede3d2ded3a31
sunnysakib <sakibur.rahaman033@gmail.com>
2024-04-22 10:12:07 +0600

    authentication using kinde

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from '../../public/assets/images/airbnb-desktop.png'
+import MobileLogo from '../../public/assets/images/airbnb-mobile.webp'
+import UserNav from "./UserNav";
+export default function Navbar () {
+    return (
+        <nav className="w-full border-b">
+            <div className="flex items-center justify-between container mx-auto lg:px-10 py-5">
+                <Link href={"/"}>
+                <Image src={DesktopLogo}
+                alt="desktop logo"
+                className="w-32 hidden lg:block"
+                />
+                 <Image src={MobileLogo}
+                alt="mobile logo"
+                className="w-12 block lg:hidden"
+                />
+                </Link>
+                
+                <div className=" rounded-full border px-5 py-2">
+                    Airbnb search
+                </div>
+                <UserNav/>
+            </div>
+        </nav>
+    );
+};
